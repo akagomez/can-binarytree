7f5ac6244ea521c69a4c142c46e86578f5ba599b
Chris Gomez <chris_gomez@bitovi.com>
2015-07-09 01:24:03 -0400

    Simplify tests

--- test/test.js
+++ test/test.js
@@ -5,37 +5,10 @@ QUnit.module('can-redblacktree', {
     setup: function () {}
 });
 
-test('Sort by key', function () {
-    var comparator = function (a, b) {
-        a = a.sourceIndex;
-        b = b.sourceIndex;
-        return a === b ? 0 : a < b ? -1 : 1; // ASC
-    };
-
-    var tree = new RBTree(comparator);
-
-    var sourceList = ['A', 'B', 'C'];
-
-    var derivedList = [
-        { value: 'B', sourceIndex: 1 },
-        { value: 'A', sourceIndex: 0 },
-        { value: 'C', sourceIndex: 2 }
-    ];
-
-    derivedList.forEach(function (item, index) {
-        tree.insert(item);
-    });
-
-    tree.each(function (item) {
-        equal(sourceList[item.sourceIndex], item.value,
-            'Value is in the correct position');
-    });
-});
-
 test('Return index on insert (without rebalance)', 3, function () {
     var comparator = function (a, b) {
-        a = a.value.charCodeAt(0);
-        b = b.value.charCodeAt(0);
+        a = a.charCodeAt(0);
+        b = b.charCodeAt(0);
         return a === b ? 0 : a < b ? -1 : 1; // ASC
     };
 
@@ -59,23 +32,17 @@ test('Return index on insert (without rebalance)', 3, function () {
         return _doubleRotate.apply(tree, arguments);
     };
 
-    var items = [
-        { value: 'B', insertIndex: 0 },
-        { value: 'A', insertIndex: 0 },
-        { value: 'C', insertIndex: 2 }
-    ];
+    var items = ['B','A','C'];
 
-    items.forEach(function (item, index) {
-        var insertIndex = tree.insert(item);
-
-        equal(insertIndex, item.insertIndex, 'Insert index reported correctly');
-    });
+    equal(tree.insert(items[0]), 0, 'Insert index reported correctly');
+    equal(tree.insert(items[1]), 0, 'Insert index reported correctly');
+    equal(tree.insert(items[2]), 2, 'Insert index reported correctly');
 });
 
-test('Return index on insert (with rebalance, singleRotate only)', 9, function () {
+test('Return index on insert (with rebalance, singleRotate only)', 5, function () {
     var comparator = function (a, b) {
-        a = a.value.charCodeAt(0);
-        b = b.value.charCodeAt(0);
+        a = a.charCodeAt(0);
+        b = b.charCodeAt(0);
         return a === b ? 0 : a < b ? -1 : 1; // ASC
     };
 
@@ -104,29 +71,18 @@ test('Return index on insert (with rebalance, singleRotate only)', 9, function (
         return _doubleRotate.apply(tree, arguments);
     };
 
-    var items = [
-        { value: 'A', insertIndex: 0 },
-        { value: 'B', insertIndex: 1 },
-        { value: 'C', insertIndex: 2 },
-        { value: 'D', insertIndex: 3 },
-        { value: 'E', insertIndex: 4 },
-        { value: 'F', insertIndex: 5 },
-        { value: 'G', insertIndex: 6 }
-    ];
+    var items = ['A','B','C'];
 
-    items.forEach(function (item, index) {
-        var insertIndex = tree.insert(item);
-        equal(insertIndex, item.insertIndex, 'Returned correct insert index');
-    });
-
-    var insertIndex = tree.insert(items[0]);
-    equal(insertIndex, -1, 'Returned "not inserted" value');
+    equal(tree.insert(items[0]), 0, 'Returned correct insert index');
+    equal(tree.insert(items[1]), 1, 'Returned correct insert index');
+    equal(tree.insert(items[2]), 2, 'Returned correct insert index');
+    equal(tree.insert(items[0]), -1, 'Returned "not inserted" value');
 });
 
 test('Return index on insert (with rebalance, single and doubleRotate)', 6, function () {
     var comparator = function (a, b) {
-        a = a.value.charCodeAt(0);
-        b = b.value.charCodeAt(0);
+        a = a.charCodeAt(0);
+        b = b.charCodeAt(0);
         return a === b ? 0 : a < b ? -1 : 1; // ASC
     };
 
@@ -159,50 +115,31 @@ test('Return index on insert (with rebalance, single and doubleRotate)', 6, func
         return result;
     };
 
-    var items = [
-        { value: 'A', insertIndex: 0 },
-        { value: 'C', insertIndex: 1 },
-        { value: 'B', insertIndex: 1 }
-    ];
+    var items = ['A','C','B'];
 
-    items.forEach(function (item, index) {
-        var insertIndex = tree.insert(item);
-        equal(insertIndex, item.insertIndex, 'Returned correct insert index');
-    });
-
-    var insertIndex = tree.insert(items[0]);
-    equal(insertIndex, -1, 'Returned "not inserted" value');
+    equal(tree.insert(items[0]), 0, 'Returned correct insert index');
+    equal(tree.insert(items[1]), 1, 'Returned correct insert index');
+    equal(tree.insert(items[2]), 1, 'Returned correct insert index');
+    equal(tree.insert(items[0]), -1, 'Returned "not inserted" value');
 });
 
 test('Return index on remove', function () {
     var comparator = function (a, b) {
-        a = a.value.charCodeAt(0);
-        b = b.value.charCodeAt(0);
+        a = a.charCodeAt(0);
+        b = b.charCodeAt(0);
         return a === b ? 0 : a < b ? -1 : 1; // ASC
     };
 
     var tree = new RBTree(comparator);
 
-    var items = [
-        { value: 'A', removedIndex: 0 },
-        { value: 'B', removedIndex: 1 },
-        { value: 'C', removedIndex: 2 },
-        { value: 'D', removedIndex: 3 },
-        { value: 'E', removedIndex: 3 },
-        { value: 'F', removedIndex: 3 },
-        { value: 'G', removedIndex: 3 }
-    ];
+    var items = ['A','B','C'];
 
-    items.forEach(function (item, index) {
-        tree.insert(item);
-    });
+    tree.insert(items[0]);
+    tree.insert(items[1]);
+    tree.insert(items[2]);
 
-    [3, 4, 5, 6, 2, 1, 0].forEach(function (itemIndex) {
-        var item = items[itemIndex];
-        var removedIndex = tree.remove(item);
-        equal(removedIndex, item.removedIndex, 'Returned correct removal index')
-        tree.print();
-    });
-
-    equal(tree.remove(items[0]), -1, 'Returned correct "not found" value');
+    equal(tree.remove(items[1]), 1, 'Returned correct remove index');
+    equal(tree.remove(items[0]), 0, 'Returned correct remove index');
+    equal(tree.remove(items[2]), 0, 'Returned correct remove index');
+    equal(tree.remove(items[0]), -1, 'Returned "not found" value');
 });
